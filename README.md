352c46b7ed1eb0693a91ae71d8e59c1a4f94ac63
suinua <maatinosippo@gmail.com>
2020-06-16 13:57:24 +0900

    update

--- README.md
+++ README.md
@@ -2,55 +2,39 @@
 スコアボードを簡単に実装できます
 
 ```php
-use scoreboard_system\ScoreboardSystem;
-use scoreboard_system\models\Score;
-use scoreboard_system\models\Scoreboard;
-use scoreboard_system\models\ScoreboardSlot;
-use scoreboard_system\models\ScoreSortType;
-
-class LobbyScoreboard extends Scoreboard
+class PlayerStatusScoreboard extends Scoreboard
 {
-    public function __construct(int $count) {
+    private static function create(string $name, int $money, int $level): Scoreboard {
         $slot = ScoreboardSlot::sideBar();
         $scores = [
             new Score($slot, "=========", 0, 0),
-            new Score($slot, "参加人数:" . $count, 1, 1),
+            new Score($slot, "Name:" . $name, 1, 1),
+            new Score($slot, "Money:" . $money, 2, 2),
+            new Score($slot, "Level:" . $level, 3, 3),
         ];
-        parent::__construct($slot, "MineDeepRock", $scores, ScoreSortType::smallToLarge());
+        return parent::__create($slot, "Server Name", $scores, ScoreSortType::smallToLarge());
+    }
+
+    static function send(Player $player, int $money, int $level) {
+        $scoreboard = self::create($player->getName(), $money, $level);
+        parent::__send($player, $scoreboard);
+    }
+
+    static function update(Player $player, int $money, int $level) {
+        $scoreboard = self::create($player->getName(), $money, $level);
+        parent::__update($player, $scoreboard);
     }
 }
+/***
+$player = null;
+$money = 1000;
+$level = 5;
+PlayerStatusScoreboard::send($player, $money, $level);
+$money = 2000;
+PlayerStatusScoreboard::update($player, $money, $level);
 
-ScoreboardSystem::setScoreboard($player, new LobbyScoreboard($count));
-```
-
-### メソッド
-
-set scoreboard
-```php
-ScoreboardSystem::setScoreboard($player,$scoreboard);
-```
-
-delete scoreboard
-```php
-ScoreboardSystem::deleteScoreboard($player,$scoreboard);
-```
-
-update scoreboard
-```php
-ScoreboardSystem::updateScoreboard($player,$scoreboard);
-```
-
-set score
-```php
-ScoreboardSystem::setScore($player,$score);
-```
-
-delete score
-```php
-ScoreboardSystem::deleteScore($player,$score);
-```
-
-update score
-```php
-ScoreboardSystem::updateScore($player,$score);
+PlayerStatusScoreboard::addScore($player, "ようこそ", 4, 4);
+PlayerStatusScoreboard::updateScore($player, "ようこそ○○サーバーへ", 4, 4);
+PlayerStatusScoreboard::deleteScore($player, 4);
+***?
 ```
--- src/scoreboard_system/models/Scoreboard.php
+++ src/scoreboard_system/models/Scoreboard.php
@@ -3,58 +3,136 @@
 namespace scoreboard_system\models;
 
 
+use pocketmine\network\mcpe\protocol\RemoveObjectivePacket;
+use pocketmine\network\mcpe\protocol\SetDisplayObjectivePacket;
+use pocketmine\network\mcpe\protocol\SetScorePacket;
+use pocketmine\network\mcpe\protocol\types\ScorePacketEntry;
+use pocketmine\Player;
+
 class Scoreboard
 {
     /**
      * @var ScoreboardSlot
      */
-    protected $slot;
+    static protected $slot;
     /**
      * @var string
      */
-    protected $title;
+    static protected $title;
     /**
      * @var ScoreSortType
      */
-    protected $sortType;
+    static protected $sortType;
 
     /**
      * @var Score[]
      */
-    private $scores;
+    static protected $scores;
 
     public function __construct(ScoreboardSlot $slot, string $title, array $scores, ScoreSortType $sortType) {
-        $this->slot = $slot;
-        $this->title = $title;
-        $this->sortType = $sortType;
-        $this->scores = $scores;
+        self::$slot = $slot;
+        self::$title = $title;
+        self::$sortType = $sortType;
+        self::$scores = $scores;
+    }
+
+    protected static function __create(ScoreboardSlot $slot, string $title, array $scores, ScoreSortType $sortType): Scoreboard {
+        return new Scoreboard($slot, $title, $scores, $sortType);
+    }
+
+    static function __send(Player $player, Scoreboard $scoreboard): void {
+        $pk = new SetDisplayObjectivePacket();
+        $pk->displaySlot = $scoreboard->getSlot()->getText();
+        $pk->objectiveName = $scoreboard->getSlot()->getText();
+        $pk->displayName = $scoreboard->getTitle();
+        $pk->criteriaName = "dummy";
+        $pk->sortOrder = $scoreboard->getSortType()->getValue();
+        $player->sendDataPacket($pk);
+
+        foreach ($scoreboard->getScores() as $score) {
+            self::addScore($player, $score->getText(), $score->getValue(), $score->getId());
+        }
+    }
+
+    static function __update(Player $player, Scoreboard $scoreboard) {
+        self::delete($player);
+        self::__send($player, $scoreboard);
+    }
+
+    static function delete(Player $player): void {
+        $pk = new RemoveObjectivePacket();
+        $pk->objectiveName = self::$slot->getText();
+        $player->sendDataPacket($pk);
+    }
+
+    static function addScore(Player $player, string $text, int $value, int $id): void {
+        $score = new Score(self::$slot, $text, $value, $id);
+        self::$scores[] = $score;
+
+        $entry = new ScorePacketEntry();
+        $entry->objectiveName = $score->getSlot()->getText();
+        $entry->type = $entry::TYPE_FAKE_PLAYER;
+        $entry->customName = $score->getText();
+        $entry->score = $score->getValue();
+        $entry->scoreboardId = $score->getId();
+
+        $pk = new SetScorePacket();
+        $pk->type = $pk::TYPE_CHANGE;
+        $pk->entries[] = $entry;
+        $player->sendDataPacket($pk);
+    }
+
+    static function deleteScore(Player $player, int $targetId): void {
+        /** @var Score $targetScore */
+
+        $tempScores = self::$scores;
+        self::$scores = [];
+        foreach ($tempScores as $tempScore) {
+            if ($tempScore->getId() !== $targetId) {
+                self::$scores[] = $tempScore;
+            }
+        }
+
+        $entry = new ScorePacketEntry();
+        $entry->objectiveName = self::$slot->getText();
+        $entry->scoreboardId = $targetId;
+
+        $pk = new SetScorePacket();
+        $pk->type = $pk::TYPE_REMOVE;
+        $pk->entries[] = $entry;
+        $player->sendDataPacket($pk);
+    }
+
+    static function updateScore(Player $player, string $text, int $value, int $id) {
+        self::deleteScore($player, $id);
+        self::addScore($player, $text, $value, $id);
     }
 
     /**
      * @return ScoreboardSlot
      */
     public function getSlot(): ScoreboardSlot {
-        return $this->slot;
+        return self::$slot;
     }
 
     /**
      * @return string
      */
     public function getTitle(): string {
-        return $this->title;
+        return self::$title;
     }
 
     /**
      * @return ScoreSortType
      */
     public function getSortType(): ScoreSortType {
-        return $this->sortType;
+        return self::$sortType;
     }
 
     /**
      * @return Score[]
      */
     public function getScores(): array {
-        return $this->scores;
+        return self::$scores;
     }
 }
